70292c213b1cbec11d3ad6ea1d66afce2c2e9f2a
rohit-testing-ml <84795510+rohit-testing-ml@users.noreply.github.com>
2021-05-26 12:50:46 +0530

    Update download.py

--- download.py
+++ download.py
@@ -1,5 +1,5 @@
 import gdown
 def download_from_drive():
-    url = 'https://drive.google.com/uc?id=1lPiJ05zxv4Nepq2PNTKv8Onyf72rXfBL'
-    output = 'BestModel'
+    url = 'https://drive.google.com/uc?id=1OFU0jAaUIhvWS3MSIlZmvxfYVGCOjzwO'
+    output = './BestModel1'
     gdown.download(url, output, quiet=False)
